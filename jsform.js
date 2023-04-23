73866d73d66389319038d35f6da9aef59d19c0eb
ApVen77 <42076342+ApVen77@users.noreply.github.com>
2023-04-22 20:02:16 -0600

    haven't pushed in awhile

--- jsform.js
+++ jsform.js
@@ -1,26 +1,11 @@
-// Add a script tag to the bottom of the page for your code.
-// Add an event listener to the button so that it calls a makeMadLib function when clicked.
-// In the makeMadLib function, retrieve the current values of the form input elements, make a story from them, and output that in the story div (like "Pamela really likes pink cucumbers.")
-
-function libIt() {
-    let storyDiv= document.getElementById("story"); 
-    let person= document.getElementById("person").value; 
-    let adjective= document.getElementById("adjective").value; 
-    let noun=document.getElementById("noun").value; 
-
-
-    storyDiv.innerHTML=  (`${person} fell off a ${adjective} ${noun} . It's funny right!!`); 
-
+function calculateDogAge(humanYears) {
+  var humanYears = document.getElementById("solution").value;
+  var dogYears = 7 * humanYears;
+  console.log("Your doggie is " + dogYears + " years old in dog years!");
+  document.getElementById("solution1").innerHTML =
+    "Your doggie is " + dogYears + " years old </br> in dog years!";
 }
 
-let button= document.getElementById("lib-button"); 
-//addeventlistener keyword (event, function)
-button.addEventListener('click', libIt); 
-
-document.getElementById("story").style.color="orange"; 
-document.getElementById("story").style.fontSize="30px"; 
-document.getElementById("story").style.fontFamily="cursive"; 
-document.getElementById("story").style.marginLeft="50px"; 
-
-
- 
+calculateDogAge(1);
+calculateDogAge(2);
+calculateDogAge(5);
